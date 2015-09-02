507fe852c9946c251fd372d8c8957c6b4b944055
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-02 21:53:47 +0300

    Credentials model

---
+++ eBFMN/eBFMN/Classes/Models/User Credentials/BFMUserCredentials.h
@@ -0,0 +1,13 @@
+//
+//  BFMUserCredentials.h
+//  eBFMN
+//
+//  Created by Mikhail Timoscenko on 02.09.15.
+//  Copyright (c) 2015 eBFMN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BFMUserCredentials : NSObject
+
+@end
---
+++ eBFMN/eBFMN/Classes/Models/User Credentials/BFMUserCredentials.m
@@ -0,0 +1,13 @@
+//
+//  BFMUserCredentials.m
+//  eBFMN
+//
+//  Created by Mikhail Timoscenko on 02.09.15.
+//  Copyright (c) 2015 eBFMN. All rights reserved.
+//
+
+#import "BFMUserCredentials.h"
+
+@implementation BFMUserCredentials
+
+@end
