d70955183c39df421573f835fda3e888abdfe5a7
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-02 21:57:56 +0300

    color and keychain categories

---
+++ eBFMN/eBFMN/Misc/Categories/JNKeychain+UNTExtension/JNKeychain+UNTExtension.h
@@ -0,0 +1,17 @@
+//
+//  JNKeychain+UNTExtension.h
+//  eBFMN
+//
+//  Created by Mikhail Timoscenko on 02.09.15.
+//  Copyright (c) 2015 eBFMN. All rights reserved.
+//
+
+#import "JNKeychain.h"
+
+extern NSString* const kUNTUsernameKey;
+extern NSString* const kUNTPasswordKey;
+extern NSString* const kUNTGUIDKey;
+
+@interface JNKeychain (UNTExtension)
+
+@end
---
+++ eBFMN/eBFMN/Misc/Categories/UIColor+UNTExtension/UIColor+Extensions.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extensions.h
+//  eBFMN
+//
+//  Created by Mikhail Timoscenko on 02.09.15.
+//  Copyright (c) 2015 eBFMN. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Extensions)
+
++ (UIColor *)colorWithR:(uint)r G:(uint)g B:(uint)b A:(uint)a;
++ (UIColor *)colorWithR:(uint)r G:(uint)g B:(uint)b;
+
+@end
+
