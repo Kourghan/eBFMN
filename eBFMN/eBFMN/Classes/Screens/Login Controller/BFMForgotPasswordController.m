e177cd485e41f7eced3125e9f1fb953432502d2a
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-25 02:24:36 +0300

    iboutlets

--- eBFMN/eBFMN/Classes/Screens/Login Controller/BFMForgotPasswordController.m
+++ eBFMN/eBFMN/Classes/Screens/Login Controller/BFMForgotPasswordController.m
@@ -8,6 +8,41 @@
 
 #import "BFMForgotPasswordController.h"
 
+@interface BFMForgotPasswordController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextfield;
+
+
+@end
+
 @implementation BFMForgotPasswordController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    self.usernameTextfield.layer.borderColor = [[UIColor whiteColor] CGColor];
+    self.usernameTextfield.layer.borderWidth = 1.f;
+    
+    self.usernameTextfield.attributedPlaceholder = [[NSAttributedString alloc] initWithString:self.usernameTextfield.placeholder
+                                                                                   attributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
+}
+
+#pragma mark - handlers
+
+- (IBAction)getPassword:(id)sender {
+    
+}
+
+- (IBAction)contactUs:(id)sender {
+    
+}
+
+- (IBAction)termsAndConditions:(id)sender {
+    
+}
+
+- (IBAction)back:(id)sender {
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
