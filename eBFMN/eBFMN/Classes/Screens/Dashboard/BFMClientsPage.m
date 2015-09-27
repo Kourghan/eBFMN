07e86187b7353ed94486c795634e65b4e8ac8e7b
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-27 19:52:40 +0300

    connect clients to User

--- eBFMN/eBFMN/Classes/Screens/Dashboard/BFMClientsPage.m
+++ eBFMN/eBFMN/Classes/Screens/Dashboard/BFMClientsPage.m
@@ -8,6 +8,41 @@
 
 #import "BFMClientsPage.h"
 
+#import "BFMUser+Extension.h"
+
+@interface BFMClientsPage ()
+
+@property (weak, nonatomic) IBOutlet UILabel *clientsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalClientsValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *liveClientsValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *liveClientsLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *demoValueClientsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *demoAccountsLabel;
+
+@end
+
 @implementation BFMClientsPage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.clientsLabel.text = NSLocalizedString(@"dashboard.title.clients", nil);
+    self.demoAccountsLabel.text = NSLocalizedString(@"dashboard.demoaccounts", nil);
+    self.liveClientsLabel.text = NSLocalizedString(@"dashboard.liveaccounts", nil);
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self bindUser:[BFMUser currentUser]];
+}
+
+- (void)bindUser:(BFMUser *)user {
+    self.totalClientsValueLabel.text = [user totalAccountsStringValue];
+    self.demoValueClientsLabel.text = [user demoAccountsStringValue];
+    self.liveClientsValueLabel.text = [user liveAccountsStringValue];
+}
 
 @end
