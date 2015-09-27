adb2672e03aa5fc3ba2ef10df11282b04128833f
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-27 18:38:27 +0300

    dashboard UI

--- eBFMN/eBFMN/Classes/Screens/Dashboard/BFMDashboardViewController.m
+++ eBFMN/eBFMN/Classes/Screens/Dashboard/BFMDashboardViewController.m
@@ -10,6 +10,9 @@
 #import "BFMEarningsPage.h"
 #import "BFMDashboardAdaptor.h"
 
+#import "NINavigationAppearance.h"
+#import "BFMDefaultNavagtionBarAppearance.h"
+
 @interface BFMDashboardViewController ()
 
 @property (nonatomic, strong) UIPageViewController *pageController;
@@ -17,6 +20,21 @@
 @end
 
 @implementation BFMDashboardViewController
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [NINavigationAppearance pushAppearanceForNavigationController:self.navigationController];
+    
+    self.navigationItem.title = NSLocalizedString(@"dashboard.title", nil);
+    [BFMDefaultNavagtionBarAppearance applyTo:self.navigationController.navigationBar];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    [NINavigationAppearance popAppearanceForNavigationController:self.navigationController];
+}
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([segue.identifier isEqualToString:@"embed"]) {
