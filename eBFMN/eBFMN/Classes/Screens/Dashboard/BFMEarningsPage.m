20692d9ff40ee604300fba99a5227f53d00ba37d
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-27 16:15:34 +0300

    center content offset

--- eBFMN/eBFMN/Classes/Screens/Dashboard/BFMEarningsPage.m
+++ eBFMN/eBFMN/Classes/Screens/Dashboard/BFMEarningsPage.m
@@ -8,6 +8,8 @@
 
 #import "BFMEarningsPage.h"
 
+#import "BFMUser+Extension.h"
+
 @interface BFMEarningsPage ()
 
 @property (weak, nonatomic) IBOutlet UILabel *reabtesValueLabel;
@@ -23,10 +25,47 @@
 @property (weak, nonatomic) IBOutlet UILabel *monthLabel;
 
 @property (weak, nonatomic) IBOutlet UIButton *currencyButton;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
 @end
 
 @implementation BFMEarningsPage
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    
+    dateFormatter.locale = [NSLocale currentLocale];
+    
+    dateFormatter.dateFormat = @"MMMM";
+    NSString * monthString = [[dateFormatter stringFromDate:[NSDate date]] uppercaseString];
+    self.monthLabel.text = monthString;
+    
+    self.earningsLabel.text = NSLocalizedString(@"dashboard.title.earnings", nil);
+    self.reabatesLabel.text = NSLocalizedString(@"dashboard.rebates", nil);
+    self.commissionLabel.text = NSLocalizedString(@"dashboard.commissions", nil);
+    self.spreadLabel.text = NSLocalizedString(@"dashboard.spread", nil);
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self bindUser:[BFMUser currentUser]];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    
+    CGFloat newContentOffsetX = (self.scrollView.contentSize.width - self.scrollView.frame.size.width) / 2;
+    self.scrollView.contentOffset = CGPointMake(newContentOffsetX, 0);
+}
+
+- (void)bindUser:(BFMUser *)user {
+    self.commissionValueLabel.text = [user.commissions stringValue];
+    self.spreadValueLabel.text = [[user spread] stringValue];
+    self.reabtesValueLabel.text = [[user spread] stringValue];
+}
 
 #pragma mark - handlers
 
