6466e78b9be4307314df49f2b7e11a038d6acac0
Mikhaylo Tymoshchenko <morbop@gmail.com>
2015-09-27 15:27:30 +0300

    outlets for page

--- eBFMN/eBFMN/Classes/Screens/Dashboard/BFMEarningsPage.m
+++ eBFMN/eBFMN/Classes/Screens/Dashboard/BFMEarningsPage.m
@@ -8,6 +8,31 @@
 
 #import "BFMEarningsPage.h"
 
+@interface BFMEarningsPage ()
+
+@property (weak, nonatomic) IBOutlet UILabel *reabtesValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *reabatesLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *commissionValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *commissionLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *spreadValueLabel;
+@property (weak, nonatomic) IBOutlet UILabel *spreadLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *earningsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *monthLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *currencyButton;
+
+@end
+
 @implementation BFMEarningsPage
+
+#pragma mark - handlers
+
+
+- (IBAction)currencyTapped:(id)sender {
+    
+}
 
 @end
