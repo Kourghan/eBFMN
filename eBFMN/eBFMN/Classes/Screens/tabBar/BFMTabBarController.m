9e04ee903c3689486e164322b888a484376b90df
Mikhaylo Tymoshchenko <morbop@gmail.com>
2016-04-02 21:28:33 +0300

    ui improvements

--- eBFMN/eBFMN/Classes/Screens/tabBar/BFMTabBarController.m
+++ eBFMN/eBFMN/Classes/Screens/tabBar/BFMTabBarController.m
@@ -21,20 +21,27 @@
     self.selectedIndex = 0;
     
     self.tabBar.tintColor = [UIColor whiteColor];
+	
+	[[UITabBarItem appearance] setTitleTextAttributes:@{
+														NSFontAttributeName:[UIFont fontWithName:@"ProximaNova-Regular" size:8.f]
+														} forState:UIControlStateNormal];
 }
 
 - (void)configureTabBarItems {
     UINavigationController *vc1 = [[UIStoryboard dashboardStoryboard] instantiateInitialViewController];
     vc1.tabBarItem.image = [UIImage imageNamed:@"ic_tabBar_dashboard"];
     vc1.tabBarItem.title = NSLocalizedString(@"tabbar.dashboard", nil);
-    
+	[vc1.tabBarItem setTitlePositionAdjustment:UIOffsetMake(0, -6)];
+	
     UINavigationController *vc2 = [[UIStoryboard leaderboardStoryboard] instantiateInitialViewController];
     vc2.tabBarItem.image = [UIImage imageNamed:@"ic_tabBar_leaderboard"];
     vc2.tabBarItem.title = NSLocalizedString(@"tabbar.leaderboard", nil);
+	[vc2.tabBarItem setTitlePositionAdjustment:UIOffsetMake(0, -6)];
     
     UINavigationController *vc3 = [[UIStoryboard newsStoryboard] instantiateInitialViewController];
     vc3.tabBarItem.image = [UIImage imageNamed:@"ic_tabBar_news"];
-    vc3.tabBarItem.title = NSLocalizedString(@"tabbar.news", nil);
+	[vc3.tabBarItem setTitlePositionAdjustment:UIOffsetMake(0, -6)];
+
 //    
 //    UINavigationController *vc4 = [[UIStoryboard notificationsStoryboard] instantiateInitialViewController];
 //    vc4.tabBarItem.image = [UIImage imageNamed:@"ic_tabBar_notifications"];
@@ -43,7 +50,8 @@
     UINavigationController *vc5 = [[UIStoryboard profileStoryboard] instantiateInitialViewController];
     vc5.tabBarItem.image = [UIImage imageNamed:@"ic_tabBar_profile"];
     vc5.tabBarItem.title = NSLocalizedString(@"tabbar.profile", nil);
-    
+    [vc5.tabBarItem setTitlePositionAdjustment:UIOffsetMake(0, -6)];
+	
     self.viewControllers = @[vc1, vc2, vc3, vc5];
 }
 
